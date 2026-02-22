f3267e681920eb68f8fdb8ee77365b12e45abd11
fatsyaalfin15 <fatsyaalvin@gmail.com>
2026-02-22 17:18:04 +0700

    Ci error2

--- src/tests/reducers/threadsSlice.test.js
+++ src/tests/reducers/threadsSlice.test.js
@@ -20,10 +20,6 @@ describe('threadsSlice reducer', () => {
     expect(nextState.data).toEqual(threads);
   });
 
-  test('CI ERROR sengaja', () => {
-  expect(1).toBe(2);
-});
-
   test('should handle fetchThreads rejected', () => {
     const action = {
       type: fetchThreads.rejected.type,
